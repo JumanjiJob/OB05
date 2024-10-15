70f7ff2a87ee878f263bcdf77490391e348153a0
JumanjiJob <35mirzhan@mail.ru>
2024-10-15 14:51:49 +0300

    add pause

--- HW_OB05.py
+++ HW_OB05.py
@@ -1,12 +1,13 @@
 import pygame
 import sys
+
 pygame.init()
 
 screen_width, screen_height = 600, 400
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Аэрохоккей для двоих")
 
-#Счетчики выйгранных очков
+# Счетчики выигранных очков
 player1 = 0
 player2 = 0
 
@@ -19,7 +20,7 @@ paddle1_speed = 20
 # Параметры платформы 2
 paddle2_width, paddle2_height = 100, 10
 paddle2_x = (screen_width - paddle2_width) // 2  # Центрирование платформы
-paddle2_y = 30 # Положение платформы 2
+paddle2_y = 30  # Положение платформы 2
 
 # Параметры мяча
 ball_radius = 8
@@ -32,11 +33,14 @@ ball_speed_y = -4
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 255, 255)
-GREEN = (0,255,0)
-RED = (255,0,0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
 clock = pygame.time.Clock()
 fps = 60
+
+# Флаг паузы
+paused = False
 
 # Запуск основного цикла игры
 run = True
@@ -45,43 +49,48 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+        # Проверка нажатия клавиши P для паузы
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_p:
+                paused = not paused  # Переключаем состояние паузы
+
+    if not paused:  # Игра продолжается
         # Движение мяча
-    ball_x += ball_speed_x
-    ball_y += ball_speed_y
+        ball_x += ball_speed_x
+        ball_y += ball_speed_y
 
-    # Столкновение с краями экрана
-    if ball_x <= 0 or ball_x >= screen_width:
-        ball_speed_x = -ball_speed_x
+        # Столкновение с краями экрана
+        if ball_x <= 0 or ball_x >= screen_width:
+            ball_speed_x = -ball_speed_x
 
-    # Подсчет выйгранных очков
-    if ball_y <= 0:
-        ball_x = screen_width // 2
-        ball_y = screen_height // 2
-        player1 += 1
-    if ball_y >= screen_height:
-        player2 += 1
-        ball_x = screen_width // 2
-        ball_y = screen_height // 2
+        # Подсчет выигранных очков
+        if ball_y <= 0:
+            ball_x = screen_width // 2
+            ball_y = screen_height // 2
+            player1 += 1
+        if ball_y >= screen_height:
+            player2 += 1
+            ball_x = screen_width // 2
+            ball_y = screen_height // 2
 
-    #Управление движением платформы1 с помощью клавиатуры
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and paddle1_x > 0:
-        paddle1_x -= paddle1_speed
-    if keys[pygame.K_RIGHT] and paddle1_x < screen_width - paddle1_width:
-        paddle1_x += paddle1_speed
+        # Управление движением платформы1 с помощью клавиатуры
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and paddle1_x > 0:
+            paddle1_x -= paddle1_speed
+        if keys[pygame.K_RIGHT] and paddle1_x < screen_width - paddle1_width:
+            paddle1_x += paddle1_speed
 
-    # Управление движением платформы2 с помощью мыши
-    if event.type == pygame.MOUSEMOTION:
-        mouseX, mouseY = pygame.mouse.get_pos()
-        paddle2_x = mouseX - 40
+        # Управление движением платформы2 с помощью мыши
+        if event.type == pygame.MOUSEMOTION:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            paddle2_x = mouseX - paddle2_width // 2
 
-    # Столкновение с платформой
-    if paddle1_x <= ball_x <= paddle1_x + paddle1_width and paddle1_y <= ball_y + ball_radius <= paddle1_y + paddle1_height:
-        ball_speed_y = -ball_speed_y
+        # Столкновение с платформами
+        if paddle1_x <= ball_x <= paddle1_x + paddle1_width and paddle1_y <= ball_y + ball_radius <= paddle1_y + paddle1_height:
+            ball_speed_y = -ball_speed_y
 
-    # Столкновение с платформой
-    if paddle2_x <= ball_x <= paddle2_x + paddle2_width and paddle2_y <= ball_y + ball_radius <= paddle2_y + paddle2_height:
-        ball_speed_y = -ball_speed_y
+        if paddle2_x <= ball_x <= paddle2_x + paddle2_width and paddle2_y <= ball_y + ball_radius <= paddle2_y + paddle2_height:
+            ball_speed_y = -ball_speed_y
 
     # Очистка экрана перед каждым новым кадром
     screen.fill(BLACK)
@@ -92,12 +101,21 @@ while run:
     pygame.draw.circle(screen, BLUE, (ball_x, ball_y), ball_radius)
 
     # Отображение счета
-    font1 = pygame.font.SysFont(None, 36)
+    font1 = pygame.font.SysFont(None, 20)
     count_text1 = font1.render(f"Игрок 1: {player1}", True, WHITE)
     screen.blit(count_text1, (10, 10))
-    font2 = pygame.font.SysFont(None, 36)
+    font2 = pygame.font.SysFont(None, 20)
     count_text2 = font2.render(f"Игрок 2: {player2}", True, WHITE)
     screen.blit(count_text2, (10, 35))
+    font3 = pygame.font.SysFont(None, 20)
+    count_text3 = font3.render("Пауза - кнопка P", True, WHITE)
+    screen.blit(count_text3, (10, 60))
+
+    # Отображение текста паузы
+    if paused:
+        pause_font = pygame.font.SysFont(None, 48)
+        pause_text = pause_font.render("Игра на паузе", True, WHITE)
+        screen.blit(pause_text, (screen_width // 2 - pause_text.get_width() // 2, screen_height // 2 - pause_text.get_height() // 2))
 
     # Обновление экрана
     pygame.display.flip()
--- test.py
+++ test.py
@@ -5,79 +5,114 @@ pygame.init()
 
 screen_width, screen_height = 600, 400
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Тетрис")
+pygame.display.set_caption("Аэрохоккей для двоих")
+
+# Счетчики выигранных очков
+player1 = 0
+player2 = 0
+
+# Параметры платформы 1
+paddle1_width, paddle1_height = 100, 10
+paddle1_x = (screen_width - paddle1_width) // 2  # Центрирование платформы
+paddle1_y = screen_height - 40  # Положение платформы 1
+paddle1_speed = 20
+
+# Параметры платформы 2
+paddle2_width, paddle2_height = 100, 10
+paddle2_x = (screen_width - paddle2_width) // 2  # Центрирование платформы
+paddle2_y = 30  # Положение платформы 2
+
+# Параметры мяча
+ball_radius = 8
+ball_x = screen_width // 2  # Начальное положение мяча по X
+ball_y = screen_height // 2  # Начальное положение мяча по Y
+ball_speed_x = 4
+ball_speed_y = -4
 
 # Цвета
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+BLUE = (0, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
 clock = pygame.time.Clock()
 fps = 60
 
-# Параметры платформы
-paddle_width, paddle_height = 100, 40
-paddle_speed = 6
-
-# Начальная позиция платформы
-def create_new_paddle():
-    return (0, 0)  # Платформа появляется в верхней левой части экрана
-
-# Флаг остановки платформы
-platform_stopped = False
-
-# Инициализация первой платформы
-paddle_x, paddle_y = create_new_paddle()
-
-# Счетчик платформ
-paddle_count = 0
-
-# Список для сохранения предыдущих платформ
-paddles = []
+# Флаг паузы
+paused = False
 
 # Запуск основного цикла игры
-running = True
-while running:
-    # Обработка событий
+run = True
+while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            run = False
 
-    # Движение платформы
-    if not platform_stopped:
+        # Проверка нажатия клавиши P для паузы
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_p:
+                paused = not paused  # Переключаем состояние паузы
+
+    if not paused:  # Игра продолжается
+        # Движение мяча
+        ball_x += ball_speed_x
+        ball_y += ball_speed_y
+
+        # Столкновение с краями экрана
+        if ball_x <= 0 or ball_x >= screen_width:
+            ball_speed_x = -ball_speed_x
+
+        # Подсчет выигранных очков
+        if ball_y <= 0:
+            ball_x = screen_width // 2
+            ball_y = screen_height // 2
+            player1 += 1
+        if ball_y >= screen_height:
+            player2 += 1
+            ball_x = screen_width // 2
+            ball_y = screen_height // 2
+
+        # Управление движением платформы1 с помощью клавиатуры
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and paddle_x > 0:
-            paddle_x -= paddle_speed
-        if keys[pygame.K_RIGHT] and paddle_x < screen_width - paddle_width:
-            paddle_x += paddle_speed
+        if keys[pygame.K_LEFT] and paddle1_x > 0:
+            paddle1_x -= paddle1_speed
+        if keys[pygame.K_RIGHT] and paddle1_x < screen_width - paddle1_width:
+            paddle1_x += paddle1_speed
 
-        # Проверка, достигла ли платформа низа экрана
-        if paddle_y + paddle_height < screen_height:
-            paddle_y += paddle_speed  # Платформа движется вниз
-        else:
-            paddle_y = screen_height - paddle_height  # Останавливается на нижней границе
-            paddles.append((paddle_x, paddle_y))  # Добавляем остановившуюся платформу в список
-            platform_stopped = True  # Устанавливаем флаг остановки
-            paddle_count += 1  # Увеличиваем счетчик платформ
+        # Управление движением платформы2 с помощью мыши
+        if event.type == pygame.MOUSEMOTION:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            paddle2_x = mouseX - paddle2_width // 2
 
-    else:
-        # Если платформа остановилась, создаем новую платформу
-        paddle_x, paddle_y = create_new_paddle()
-        platform_stopped = False
+        # Столкновение с платформами
+        if paddle1_x <= ball_x <= paddle1_x + paddle1_width and paddle1_y <= ball_y + ball_radius <= paddle1_y + paddle1_height:
+            ball_speed_y = -ball_speed_y
+
+        if paddle2_x <= ball_x <= paddle2_x + paddle2_width and paddle2_y <= ball_y + ball_radius <= paddle2_y + paddle2_height:
+            ball_speed_y = -ball_speed_y
 
     # Очистка экрана перед каждым новым кадром
     screen.fill(BLACK)
 
-    # Рисование всех предыдущих платформ
-    for px, py in paddles:
-        pygame.draw.rect(screen, WHITE, (px, py, paddle_width, paddle_height))
+    # Рисование объектов
+    pygame.draw.rect(screen, GREEN, (paddle1_x, paddle1_y, paddle1_width, paddle1_height))
+    pygame.draw.rect(screen, RED, (paddle2_x, paddle2_y, paddle2_width, paddle2_height))
+    pygame.draw.circle(screen, BLUE, (ball_x, ball_y), ball_radius)
 
-    # Рисование текущей движущейся платформы
-    pygame.draw.rect(screen, WHITE, (paddle_x, paddle_y, paddle_width, paddle_height))
+    # Отображение счета
+    font1 = pygame.font.SysFont(None, 36)
+    count_text1 = font1.render(f"Игрок 1: {player1}", True, WHITE)
+    screen.blit(count_text1, (10, 10))
+    font2 = pygame.font.SysFont(None, 36)
+    count_text2 = font2.render(f"Игрок 2: {player2}", True, WHITE)
+    screen.blit(count_text2, (10, 35))
 
-    # Отображение счетчика платформ
-    font = pygame.font.SysFont(None, 36)
-    count_text = font.render(f"Платформы: {paddle_count}", True, WHITE)
-    screen.blit(count_text, (10, 10))
+    # Отображение текста паузы
+    if paused:
+        pause_font = pygame.font.SysFont(None, 48)
+        pause_text = pause_font.render("Игра на паузе", True, WHITE)
+        screen.blit(pause_text, (screen_width // 2 - pause_text.get_width() // 2, screen_height // 2 - pause_text.get_height() // 2))
 
     # Обновление экрана
     pygame.display.flip()
